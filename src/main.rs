3b4541653cf60ff21c4578b1dbebc013601d390c
ogiwara <yushiogiwara@gmail.com>
2019-07-19 21:41:01 +0900

    lifecycle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let str = String::from("long");
+    let result;
+
+    {
+        let str2 = String::from("xyz");
+        result = longest(str.as_str(), str2.as_str());
+    }
+
+    print!("{}", result);
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str{
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
