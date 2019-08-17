16296e1576f45bf251ae041958de51d1f3592230
ogiwara <yushiogiwara@gmail.com>
2019-08-17 16:25:05 +0900

    add union tree impl

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,52 @@
 use std::fmt;
 
+static mut par: [usize;10] = [0,1,2,3,4,5,6,7,8,9];
+static mut rank: [usize;10] = [0;10];
+
 fn main(){
-    test();
-}
-
-struct Node{
-    val: usize,
-    lch: Option<Box<Node>>,
-    rch: Option<Box<Node>>,
-}
-
-impl Node{
-    fn new(init: usize) -> Node{
-        Node {
-            val: init,
-            lch: None,
-            rch: None
-        }
+    unsafe {
+        test();
     }
+}
 
-    fn insert(self: &mut Node,x: usize){
-        if(x < self.val){
-            // insert to lch
-            match &mut self.lch{
-                None => {
-                    self.lch = Some(Box::new(Node::new(x)))
-                } 
-                Some(lch) => {
-                    lch.insert(x);
-                } 
-            }
-        }else{
-            match &mut self.rch{
-                None => {
-                    self.rch = Some(Box::new(Node::new(x)))
-                } 
-                Some(rhc) => {
-                    rhc.insert(x);
-                } 
-            }
+// 木の根を求める
+unsafe fn find(x: usize) -> usize{
+    if par[x] == x {
+        return x;
+    }else{
+        par[x] = find(par[x]);
+        return par[x];
+    }
+}
+
+// xとyの属する集合を併合
+unsafe fn unite(mut x: usize,mut y: usize){
+    x = find(x);
+    y = find(y);
+    if x == y {return;}
+
+    if rank[x] < rank[y] {
+        par[x] = y;
+    }else{
+        par[y] = x;
+        if rank[x] == rank[y] {
+            rank[x] += 1;
         }
     }
 }
 
-impl ToString for Node{
-    fn to_string(&self) -> String{
-        let lch_str = match &self.lch {
-            None => String::from(""),
-            Some(lch) => lch.to_string()
-        };
-        let rch_str = match &self.rch {
-            None => String::from(""),
-            Some(rch) => rch.to_string()
-        };
-
-        format!("{}-(l{} r{})",
-        self.val,lch_str,rch_str)
-    }
+// xとyが同じ集合に属するか否か
+unsafe fn same(x: usize, y: usize) -> bool{
+    return find(x) == find(y);
 }
 
-fn test(){
-    let mut node = Node::new(2);
-    node.insert(1);
-    node.insert(3);
-    node.insert(6);
-    node.insert(7);
-    node.insert(5);
+unsafe fn test(){
+    unite(1,2);
+    unite(1,5);
 
-    println!("{}", node.to_string());
+    unite(6,3);
+    unite(4,7);
+    unite(6,7);
+
+    println!("{}", same(6, 2));
 }
